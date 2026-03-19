f005d74f85f38186b7923f34c59d26664ff800e2
yusufgonen <yusuf.gonen@ug.bilkent.edu.tr>
2026-03-19 04:08:15 +0300

    created Effect.java

---
+++ core/src/main/java/com/td/game/entities/Effect.java
@@ -0,0 +1,49 @@
+package com.td.game.entities;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+public class Effect {
+    private Vector3 position;
+    private final Texture texture;
+    private final float maxLifetime;
+    private float currentLifetime;
+    private float scale;
+    private boolean alive;
+
+    public Effect(Vector3 position, Texture texture, float lifetime, float scale) {
+        this.position = position.cpy();
+        this.texture = texture;
+        this.maxLifetime = lifetime;
+        this.currentLifetime = lifetime;
+        this.scale = scale;
+        this.alive = true;
+    }
+
+    public void update(float delta) {
+        currentLifetime -= delta;
+        if (currentLifetime <= 0) {
+            alive = false;
+        }
+    }
+
+    public void render(SpriteBatch batch, com.badlogic.gdx.graphics.Camera camera) {
+        if (!alive || texture == null) return;
+
+        Vector3 screenPos = camera.project(position.cpy());
+        if (screenPos.z < 0 || screenPos.z > 1) return;
+
+        float alpha = currentLifetime / maxLifetime;
+        batch.setColor(1, 1, 1, alpha);
+        float w = texture.getWidth() * scale;
+        float h = texture.getHeight() * scale;
+        batch.draw(texture, screenPos.x - w / 2, screenPos.y - h / 2, w, h);
+        batch.setColor(Color.WHITE);
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+}
