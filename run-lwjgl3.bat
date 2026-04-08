0a35b4302b613208c1dbaa38b51bbf4d62d8dc62
yusufgonen <yusuf.gonen@ug.bilkent.edu.tr>
2026-04-09 01:56:59 +0300

    Tried something

--- run-lwjgl3.bat
+++ run-lwjgl3.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+if not exist "saves" mkdir "saves"
+if not exist "saves\options.json" echo {"musicVolume":0.7,"soundVolume":1.0,"fullscreen":false,"bindings":{}}>"saves\options.json"
 call .\gradlew.bat --stop
 taskkill /F /IM java.exe >nul 2>&1
 taskkill /F /IM javaw.exe >nul 2>&1
